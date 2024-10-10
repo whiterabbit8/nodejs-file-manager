a34a673921148a9b5f956d114a2e13daa1de08d0
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2024-10-10 17:56:06 +0300

    feat: implement showing working directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import readline from 'readline';
+import { homedir } from 'os';
+
 const { stdin, stdout } = process;
 
 let username;
+let workingDir = homedir();
 
 const storeName = () => {
   const params = process.argv.slice(2);
@@ -12,13 +15,17 @@ const startApp = () => {
   storeName();
   const greetingMessage = `Welcome to the File Manager, ${username}! \n`;
   const exitMessage = `Thank you for using File Manager, ${username}, goodbye! \n`;
-
+  const currentDirMessage = `You are currently in ${workingDir} \n`;
   const rl = readline.createInterface(stdin, stdout);
 
   stdout.write(greetingMessage);
+  stdout.write(currentDirMessage);
+
   rl.on('line', (data) => {
     if (data === '.exit') {
       process.exit();
+    } else {
+      stdout.write(currentDirMessage);
     }
   })
   process.on('exit', () => stdout.write(exitMessage));
